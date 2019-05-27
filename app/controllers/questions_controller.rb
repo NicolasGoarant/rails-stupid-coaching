fd28b411d65f7853dcbf18a503a441f144b51d0b
nicolas goarant <nicolas.goarant@hotmail.fr>
2019-05-27 15:28:30 +0200

    form

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @coachanswer = ""
+    if @question == "I am going to work"
+    @coachanswer = "great"
+
+    elsif @question.end_with? "?"
+     @coachanswer = "Silly question, get dressed and go to work!"
+
+    else @coachanswer = "I don't care, get dressed and go to work!"
+
+    end
+  end
+end
